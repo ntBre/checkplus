f69b4b79269018ed95c0d719d75f2a6a9678af20
Brent Westbrook <brentrwestbrook@gmail.com>
2023-06-11 16:18:29 -0400

    get a working api for stockfish

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
-use std::io::{BufRead, BufReader, Write};
-use std::process::{Child, Command, Stdio};
-use std::sync::mpsc::{sync_channel, Receiver, SyncSender};
-use std::thread::{self, JoinHandle};
+#![feature(array_chunks)]
 
-#[allow(unused)]
+use std::fmt::Display;
+use std::io::{BufRead, BufReader, Write};
+use std::process::{ChildStdin, ChildStdout, Command, Stdio};
+
+mod pgn;
+
 struct Stockfish {
-    sender: SyncSender<&'static str>,
-    output: Receiver<String>,
-    child: Child,
-    to_stockfish: JoinHandle<()>,
-    from_stockfish: JoinHandle<()>,
+    stdin: ChildStdin,
+    stdout: BufReader<ChildStdout>,
 }
 
 impl Stockfish {
@@ -20,66 +19,70 @@ impl Stockfish {
             .stdout(Stdio::piped())
             .spawn()
             .unwrap();
-        let mut stdin = child.stdin.take().unwrap();
-        let stdout = child.stdout.take().unwrap();
-        let stdout = BufReader::new(stdout);
+        let stdin = child.stdin.take().unwrap();
+        let stdout = BufReader::new(child.stdout.take().unwrap());
+        Self { stdin, stdout }
+    }
 
-        let (sender, receiver) = sync_channel(1);
+    /// write `cmd` to stockfish's stdin
+    fn send<D>(&mut self, cmd: D)
+    where
+        D: Display,
+    {
+        writeln!(self.stdin, "{}", cmd).unwrap();
+    }
 
-        // this thread sends things to stockfish
-        let to_stockfish = thread::spawn(move || {
-            for r in receiver {
-                writeln!(stdin, "{r}").unwrap();
+    /// loop over self.output until the pattern `until` is encountered and
+    /// return the resulting output
+    fn receive(&mut self, until: &'static str) -> String {
+        let mut s = String::new();
+        let mut buf = String::new();
+        while let Ok(_) = self.stdout.read_line(&mut buf) {
+            s.push_str(&buf);
+            s.push('\n');
+            if buf.starts_with(until) {
+                break;
             }
-        });
+            buf.clear();
+        }
+        return s;
+    }
 
-        let (so, output) = sync_channel(1);
-        let from_stockfish = thread::spawn(move || {
-            let mut s = String::new();
-            for line in stdout.lines().flatten() {
-                s.push_str(dbg!(&line));
-                s.push('\n');
-                if line == "readyok" {
-                    so.send(std::mem::take(&mut s)).unwrap();
+    /// set stockfish's position
+    fn set_position(&mut self, fen: &'static str) {
+        self.send(format!("position fen {fen}"));
+    }
+
+    /// score the current position to `depth`
+    fn get_score(&mut self, depth: usize) -> f64 {
+        self.send(format!("go depth {}", depth));
+        let output = self.receive("bestmove");
+        let mut score = 0.0;
+        for line in output.split('\n') {
+            if line.starts_with("info") {
+                let mut sp = line.split_ascii_whitespace();
+                // not found on line saying NNUE is enabled
+                if let Some(_) = sp.position(|s| s == "cp") {
+                    let text = sp.next().unwrap();
+                    score = text.parse::<f64>().unwrap();
                 }
             }
-        });
-
-        Self {
-            sender,
-            output,
-            child,
-            to_stockfish,
-            from_stockfish,
         }
-    }
-
-    fn send(&self, cmd: &'static str) {
-        self.sender.send(cmd).unwrap();
-        self.sender.send("isready").unwrap();
-    }
-
-    fn receive(&self) -> String {
-        self.output.recv().unwrap()
-    }
-
-    fn _quit(self) {
-        self.send("quit");
-        self.child.wait_with_output().unwrap();
-        self.to_stockfish.join().unwrap();
-        self.from_stockfish.join().unwrap();
+        // stockfish reports the score as an integer in units of centipawns
+        score / 100.0
     }
 }
 
 fn main() {
-    let stockfish = Stockfish::new();
+    let mut stockfish = Stockfish::new();
 
     stockfish.send("isready");
-    stockfish.receive();
+    stockfish.receive("readyok");
 
-    stockfish.send("position fen 8/7p/4p3/8/3k4/2p5/4R1KP/8; w - - 0 43");
-    stockfish.receive();
+    let fen = "8/7p/4p3/8/3k4/2p5/4R1KP/8; w - - 0 43";
 
-    stockfish.send("go depth 5");
-    stockfish.receive();
+    stockfish.set_position(fen);
+
+    let score = stockfish.get_score(20);
+    println!("score = {score}");
 }

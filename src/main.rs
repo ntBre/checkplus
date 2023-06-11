c475ac3016b2bbc6df1d6da3a8b8108264a588ef
Brent Westbrook <brentrwestbrook@gmail.com>
2023-06-11 16:54:12 -0400

    move stockfish to its own module and file

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,12 @@
 #![feature(array_chunks)]
 
-use std::fmt::Display;
-use std::io::{BufRead, BufReader, Write};
-use std::process::{ChildStdin, ChildStdout, Command, Stdio};
-
 use crate::board::Board;
 use crate::pgn::Pgn;
-
-mod pgn;
+use crate::stockfish::Stockfish;
 
 mod board;
-
-struct Stockfish {
-    stdin: ChildStdin,
-    stdout: BufReader<ChildStdout>,
-}
-
-impl Stockfish {
-    fn new() -> Self {
-        let mut cmd = Command::new("stockfish");
-        let mut child = cmd
-            .stdin(Stdio::piped())
-            .stdout(Stdio::piped())
-            .spawn()
-            .unwrap();
-        let stdin = child.stdin.take().unwrap();
-        let stdout = BufReader::new(child.stdout.take().unwrap());
-        Self { stdin, stdout }
-    }
-
-    /// write `cmd` to stockfish's stdin
-    fn send<D>(&mut self, cmd: D)
-    where
-        D: Display,
-    {
-        writeln!(self.stdin, "{}", cmd).unwrap();
-    }
-
-    /// loop over self.output until the pattern `until` is encountered and
-    /// return the resulting output
-    fn receive(&mut self, until: &'static str) -> String {
-        let mut s = String::new();
-        let mut buf = String::new();
-        while let Ok(_) = self.stdout.read_line(&mut buf) {
-            s.push_str(&buf);
-            s.push('\n');
-            if buf.starts_with(until) {
-                break;
-            }
-            buf.clear();
-        }
-        return s;
-    }
-
-    /// set stockfish's position
-    fn set_position(&mut self, fen: &'static str) {
-        self.send(format!("position fen {fen}"));
-    }
-
-    /// score the current position to `depth`
-    fn get_score(&mut self, depth: usize) -> f64 {
-        self.send(format!("go depth {}", depth));
-        let output = self.receive("bestmove");
-        let mut score = 0.0;
-        for line in output.split('\n') {
-            if line.starts_with("info") {
-                let mut sp = line.split_ascii_whitespace();
-                // not found on line saying NNUE is enabled
-                if let Some(_) = sp.position(|s| s == "cp") {
-                    let text = sp.next().unwrap();
-                    score = text.parse::<f64>().unwrap();
-                }
-            }
-        }
-        // stockfish reports the score as an integer in units of centipawns
-        score / 100.0
-    }
-}
+mod pgn;
+mod stockfish;
 
 fn main() {
     let pgn = Pgn::load("testfiles/sample.pgn").unwrap();
---
+++ src/stockfish.rs
@@ -0,0 +1,74 @@
+use std::fmt::Display;
+use std::io::BufReader;
+use std::io::{BufRead, Write};
+use std::process::ChildStdin;
+use std::process::ChildStdout;
+use std::process::Command;
+use std::process::Stdio;
+
+pub(crate) struct Stockfish {
+    pub(crate) stdin: ChildStdin,
+    pub(crate) stdout: BufReader<ChildStdout>,
+}
+
+impl Stockfish {
+    pub(crate) fn new() -> Self {
+        let mut cmd = Command::new("stockfish");
+        let mut child = cmd
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .spawn()
+            .unwrap();
+        let stdin = child.stdin.take().unwrap();
+        let stdout = BufReader::new(child.stdout.take().unwrap());
+        Self { stdin, stdout }
+    }
+
+    /// write `cmd` to stockfish's stdin
+    pub(crate) fn send<D>(&mut self, cmd: D)
+    where
+        D: Display,
+    {
+        writeln!(self.stdin, "{}", cmd).unwrap();
+    }
+
+    /// loop over self.output until the pattern `until` is encountered and
+    /// return the resulting output
+    pub(crate) fn receive(&mut self, until: &'static str) -> String {
+        let mut s = String::new();
+        let mut buf = String::new();
+        while let Ok(_) = self.stdout.read_line(&mut buf) {
+            s.push_str(&buf);
+            s.push('\n');
+            if buf.starts_with(until) {
+                break;
+            }
+            buf.clear();
+        }
+        return s;
+    }
+
+    /// set stockfish's position
+    pub(crate) fn set_position(&mut self, fen: &'static str) {
+        self.send(format!("position fen {fen}"));
+    }
+
+    /// score the current position to `depth`
+    pub(crate) fn get_score(&mut self, depth: usize) -> f64 {
+        self.send(format!("go depth {}", depth));
+        let output = self.receive("bestmove");
+        let mut score = 0.0;
+        for line in output.split('\n') {
+            if line.starts_with("info") {
+                let mut sp = line.split_ascii_whitespace();
+                // not found on line saying NNUE is enabled
+                if let Some(_) = sp.position(|s| s == "cp") {
+                    let text = sp.next().unwrap();
+                    score = text.parse::<f64>().unwrap();
+                }
+            }
+        }
+        // stockfish reports the score as an integer in units of centipawns
+        score / 100.0
+    }
+}

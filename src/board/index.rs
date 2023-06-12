9e1047f4849efd4cfebe427038f112e3e553b6d8
Brent Westbrook <brentrwestbrook@gmail.com>
2023-06-12 10:09:44 -0400

    subtract in index methods so that the rank number matches reality

--- src/board/index.rs
+++ src/board/index.rs
@@ -5,12 +5,14 @@ impl Index<(usize, usize)> for Board {
     type Output = Piece;
 
     fn index(&self, (i, j): (usize, usize)) -> &Self::Output {
+        let i = 7 - i;
         &self.board[i][j]
     }
 }
 
 impl IndexMut<(usize, usize)> for Board {
     fn index_mut(&mut self, (i, j): (usize, usize)) -> &mut Self::Output {
+        let i = 7 - i;
         &mut self.board[i][j]
     }
 }

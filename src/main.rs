4e2cf4dc3207b44cd855c2740b0aec920c050009
Brent Westbrook <brentrwestbrook@gmail.com>
2023-06-14 12:29:41 -0400

    add clap and parse commandline args

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![feature(array_chunks, let_chains)]
 
+use clap::{arg, value_parser, Command};
+
 use crate::board::{Board, Color};
 use crate::pgn::Pgn;
 use crate::stockfish::Stockfish;
@@ -8,10 +10,31 @@ pub mod board;
 mod pgn;
 mod stockfish;
 
-const DEPTH: usize = 20;
+struct Args {
+    depth: usize,
+    input: String,
+}
+
+impl Args {
+    fn new() -> Self {
+        let args = Command::new("checkplus")
+            .arg(
+                arg!(-d --depth <DEPTH> "Set the search depth")
+                    .value_parser(value_parser!(usize))
+                    .default_value("20"),
+            )
+            .arg(arg!(<input> "PGN file to score"))
+            .get_matches();
+        let depth = *args.get_one::<usize>("depth").unwrap();
+        let input = args.get_one::<String>("input").unwrap().to_owned();
+        Self { depth, input }
+    }
+}
 
 fn main() {
-    let pgn = Pgn::load("testfiles/sample.pgn").unwrap();
+    let args = Args::new();
+
+    let pgn = Pgn::load(args.input).unwrap();
     let mut board = Board::new();
 
     let mut stockfish = Stockfish::new();
@@ -22,7 +45,7 @@ fn main() {
     stockfish.set_position(board.fen(0));
 
     let mut cur = &Color::White;
-    let score = stockfish.get_score(DEPTH, *cur);
+    let score = stockfish.get_score(args.depth, *cur);
     println!("0 {score}");
 
     let mut to_move = [Color::Black, Color::White].iter().cycle();
@@ -33,7 +56,7 @@ fn main() {
         cur = to_move.next().unwrap();
         let fen = board.fen(i);
         stockfish.set_position(fen);
-        let score = stockfish.get_score(DEPTH, *cur);
+        let score = stockfish.get_score(args.depth, *cur);
         println!("{i} {score:.2}");
     }
 }

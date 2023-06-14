b86c83d719ce02f8a455f8c2c545991b17f517a4
Brent Westbrook <brentrwestbrook@gmail.com>
2023-06-14 12:10:31 -0400

    get rid of dbg!(fen) calls and factor DEPTH into constant

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ pub mod board;
 mod pgn;
 mod stockfish;
 
+const DEPTH: usize = 20;
+
 fn main() {
     let pgn = Pgn::load("testfiles/sample.pgn").unwrap();
     let mut board = Board::new();
@@ -17,10 +19,10 @@ fn main() {
     stockfish.send("isready");
     stockfish.receive("readyok");
 
-    stockfish.set_position(dbg!(board.fen(0)));
+    stockfish.set_position(board.fen(0));
 
     let mut cur = &Color::White;
-    let score = stockfish.get_score(20, *cur);
+    let score = stockfish.get_score(DEPTH, *cur);
     println!("0 {score}");
 
     let mut to_move = [Color::Black, Color::White].iter().cycle();
@@ -30,10 +32,8 @@ fn main() {
         board.make_move(m, *cur);
         cur = to_move.next().unwrap();
         let fen = board.fen(i);
-        stockfish.set_position(dbg!(fen));
-        let score = stockfish.get_score(20, *cur);
-        println!("{i} {score}");
+        stockfish.set_position(fen);
+        let score = stockfish.get_score(DEPTH, *cur);
+        println!("{i} {score:.2}");
     }
-
-    // let fen = "8/7p/4p3/8/3k4/2p5/4R1KP/8; w - - 0 43";
 }
